fea8b7e2b399da42df0868a4b07ecf40b7719036
truong8864 <43312736+truong8864@users.noreply.github.com>
2019-11-25 10:57:46 +0700

    Update test2.asm

--- test2.asm
+++ test2.asm
@@ -1,32 +1,8 @@
-threadFunc():
-  push rbp
-  mov rbp, rsp
-  mov eax, 10
-  pop rbp
-  ret
-add():
-  push rbp
-  mov rbp, rsp
-  sub rsp, 16
-  lea rax, [rbp-8]
-  mov esi, OFFSET FLAT:threadFunc()
-  mov rdi, rax
-  call std::thread::thread<int (&)(), , void>(int (&)())
-  lea rax, [rbp-8]
-  mov rdi, rax
-  call std::thread::~thread() [complete object destructor]
-  nop
-  leave
-  ret
-vtable for std::thread::_State_impl<std::thread::_Invoker<std::tuple<int (*)()> > >:
-  .quad 0
-  .quad typeinfo for std::thread::_State_impl<std::thread::_Invoker<std::tuple<int (*)()> > >
-  .quad std::thread::_State_impl<std::thread::_Invoker<std::tuple<int (*)()> > >::~_State_impl() [complete object destructor]
-  .quad std::thread::_State_impl<std::thread::_Invoker<std::tuple<int (*)()> > >::~_State_impl() [deleting destructor]
-  .quad std::thread::_State_impl<std::thread::_Invoker<std::tuple<int (*)()> > >::_M_run()
-typeinfo for std::thread::_State_impl<std::thread::_Invoker<std::tuple<int (*)()> > >:
-  .quad vtable for __cxxabiv1::__si_class_type_info+16
-  .quad typeinfo name for std::thread::_State_impl<std::thread::_Invoker<std::tuple<int (*)()> > >
-  .quad typeinfo for std::thread::_State
-typeinfo name for std::thread::_State_impl<std::thread::_Invoker<std::tuple<int (*)()> > >:
-  .string "NSt6thread11_State_implINS_8_InvokerISt5tupleIJPFivEEEEEEE" 
+wasm-function[0]:
+  sub rsp, 8                            ; 0x000000 48 83 ec 08
+  mov ecx, esi                          ; 0x000004 8b ce
+  mov eax, ecx                          ; 0x000006 8b c1
+  add eax, edi                          ; 0x000008 03 c7
+  nop                                   ; 0x00000a 66 90
+  add rsp, 8                            ; 0x00000c 48 83 c4 08
+  ret                                   ; 0x000010 c3
